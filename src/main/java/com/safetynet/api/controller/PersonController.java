e4c51bf4234cf47c7cf9e908552b35ac0446f756
Wardenfar <theo.emeriau@gmail.com>
2021-02-27 10:40:45 +0100

    PersonController : /personInfo

---
+++ src/main/java/com/safetynet/api/controller/PersonController.java
@@ -0,0 +1,27 @@
+package com.safetynet.api.controller;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.safetynet.api.model.Person;
+import com.safetynet.api.repository.PersonRepository;
+import com.safetynet.api.util.Views;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+public class PersonController {
+
+    PersonRepository personRepo;
+
+    public PersonController(PersonRepository personRepo) {
+        this.personRepo = personRepo;
+    }
+
+    @GetMapping("/personInfo")
+    @JsonView(Views.PublicAndPerson.class)
+    Set<Person> personInfo(@RequestParam String firstName, @RequestParam String lastName) {
+        return this.personRepo.findAllByFirstNameAndLastName(firstName, lastName);
+    }
+}

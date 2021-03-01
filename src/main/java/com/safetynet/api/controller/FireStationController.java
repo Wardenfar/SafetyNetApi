2f4f7a9abb7222b5f4a1c3f0b2b2c6e5fe55b88e
Wardenfar <theo.emeriau@gmail.com>
2021-03-01 14:19:16 +0100

    [JavaDoc] Controllers

--- src/main/java/com/safetynet/api/controller/FireStationController.java
+++ src/main/java/com/safetynet/api/controller/FireStationController.java
@@ -17,57 +17,92 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * RestController for routes related with the FireStation entity
+ */
 @RestController
 public class FireStationController {
 
     FireStationRepository fireStationRepo;
 
+    /**
+     * Constructor
+     */
     public FireStationController(FireStationRepository fireStationRepo) {
         this.fireStationRepo = fireStationRepo;
     }
 
+    /**
+     * GET /firestation?stationNumber=?
+     *
+     * Return information about this fireStation
+     */
     @GetMapping("/firestation")
     @JsonView(Views.FireStationModel.class)
     FireStationModel fireStation(@RequestParam String stationNumber) throws EntityNotFound {
+        // find the fireStation by its station number
         FireStation fireStation = this.fireStationRepo.findOneByStation(stationNumber);
-        if (fireStation == null) {
+        if (fireStation == null) { // if no fireStation found, then throw and error
             throw new EntityNotFound("FireStation not found by station number : " + stationNumber);
         }
+        // else build and return the model
         return FireStationModel.build(fireStation);
     }
 
+    /**
+     * GET /phoneAlert?stationNumber=?
+     *
+     * Return all person's phone linked to this fireStation
+     */
     @GetMapping("/phoneAlert")
     PhoneAlertModel phoneAlert(@RequestParam String stationNumber) throws EntityNotFound {
+        // find the fireStation by its station number
         FireStation fireStation = this.fireStationRepo.findOneByStation(stationNumber);
-        if (fireStation == null) {
+        if (fireStation == null) { // if no fireStation found, then throw and error
             throw new EntityNotFound("FireStation not found by station number : " + stationNumber);
         }
+        // else build and return the model
         return PhoneAlertModel.build(fireStation.getPersons());
     }
 
+    /**
+     * GET /fire?address=?
+     *
+     * Return all information about the fireStation and the persons of this address
+     */
     @GetMapping("/fire")
     @JsonView(Views.FireModel.class)
     FireModel fire(@RequestParam String address) throws EntityNotFound {
+        // find the fireStation by its station number
         FireStation fireStation = this.fireStationRepo.findOneByAddress(address);
-        if (fireStation == null) {
+        if (fireStation == null) { // if no fireStation found, then throw and error
             throw new EntityNotFound("FireStation not found by address : " + address);
         }
+        // else build and return the model
         return FireModel.build(fireStation);
     }
 
-
+    /**
+     * GET /flood/stations?station=?,?,?
+     *
+     * Return information about all asked stations
+     */
     @GetMapping("/flood/stations")
     @JsonView(Views.FloodStationsModel.class)
     FloodStationsModel floodStations(@RequestParam List<String> stations) throws EntityNotFound {
         Set<FireStation> fireStations = new HashSet<>();
+        // for each parameter
         for (String station : stations) {
+            // find the station
             FireStation fireStation = this.fireStationRepo.findOneByStation(station);
-            if (fireStation == null) {
+            if (fireStation == null) { // if one fireStation is missing : throw an error
                 throw new EntityNotFound("FireStation not found by station number : " + station);
             } else {
+                // else add it to the list
                 fireStations.add(fireStation);
             }
         }
+        // build and return the model from the list
         return FloodStationsModel.build(fireStations);
     }
 }
--- src/main/java/com/safetynet/api/controller/PersonController.java
+++ src/main/java/com/safetynet/api/controller/PersonController.java
@@ -15,103 +15,176 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
+/**
+ * Rest controller for routes related to the Person Entity
+ */
 @RestController
 public class PersonController {
-
 
     PersonRepository personRepo;
     FireStationRepository fireStationRepo;
 
+    /**
+     * Constructor
+     */
     public PersonController(PersonRepository personRepo, FireStationRepository fireStationRepo) {
         this.personRepo = personRepo;
         this.fireStationRepo = fireStationRepo;
     }
 
+    /**
+     * GET /personInfo?firstName=?&lastName=?
+     *
+     * Return information about this person
+     */
     @GetMapping("/personInfo")
     @JsonView(Views.PersonInfoModel.class)
     Person personInfo(@RequestParam String firstName, @RequestParam String lastName) throws EntityNotFound {
+        // find the person by it's firstName and lastName
         Person person = this.personRepo.findOneByFirstNameAndLastName(firstName, lastName);
-        if (person == null) {
+        if (person == null) { // if the person is null, throw an error
             throw new EntityNotFound("Person not found by firstName and lastName : " + firstName + " " + lastName);
         }
+        // else return the person
         return person;
     }
 
+    /**
+     * GET /childAlert?address=?
+     *
+     * Return all children of this address
+     */
     @GetMapping("/childAlert")
     @JsonView(Views.ChildAlertModel.class)
     ChildAlertModel childAlert(@RequestParam String address) {
+        // find all children from this address
         Set<Person> children = this.personRepo.findAllByAddress(address);
+        // build and return the model
         return ChildAlertModel.build(children, this.personRepo);
     }
 
+    /**
+     * GET /communityEmail?city=?
+     *
+     * Return all person's email from a given city
+     */
     @GetMapping("/communityEmail")
     CommunityEmailModel communityEmail(@RequestParam String city) {
+        // find all persons in the city
         Set<Person> persons = this.personRepo.findAllByCity(city);
+        // build and return the model
         return CommunityEmailModel.build(persons);
     }
 
+    /**
+     * Create a person and link it with a fireStation
+     *
+     * POST /person
+     * Json body
+     *
+     * All parameters are required @see RestPersonModel
+     */
     @PostMapping("/person")
     @JsonView(Views.RestResultModel.class)
     RestResultModel addPerson(@RequestBody RestPersonModel model) {
+        // check required parameters (firstName)
         if (model.getFirstName() == null) {
             return RestResultModel.buildError("POST", "person", "FirstName should not be null");
         }
+        // check required parameters (lastName)
         if (model.getLastName() == null) {
             return RestResultModel.buildError("POST", "person", "LastName should not be null");
         }
 
+        // Search if the person already exists
         Person existing = this.personRepo.findOneByFirstNameAndLastName(model.getFirstName(), model.getLastName());
+        // Search the fireStation
         FireStation fireStation = this.fireStationRepo.findOneByStation(model.getFireStation());
+
+        // if no person already exists and the fireStation
         if (existing == null && fireStation != null) {
             try {
+                // build the person from the model
                 Person toAdd = Person.fromModel(model);
                 toAdd.setFireStation(fireStation);
+
+                // add it to the repository
                 this.personRepo.add(toAdd);
+
+                // success
                 return RestResultModel.buildSuccess("POST", "person");
             } catch (Exception e) {
+                // Catch NullPointerExceptions (if a property is null)
                 return RestResultModel.buildError("POST", "person", e.getMessage());
             }
         } else if (existing != null) {
+            // if the person already exists
             return RestResultModel.buildError("POST", "person", "The combination of firstName and lastName should be unique");
         } else {
+            // if the fireStation don't exists
             return RestResultModel.buildError("POST", "person", "The fireStation was not found");
         }
     }
 
+    /**
+     * Update a person by it's firstName and lastName
+     *
+     * PUT /person
+     * Json body
+     *
+     * Two required parameters : firstName, lastName
+     * All others are the same as for POST but they are optional
+     */
     @PutMapping("/person")
     @JsonView(Views.RestResultModel.class)
     RestResultModel updatePerson(@RequestBody RestPersonModel model) {
+        // Check required parameters (firstName)
         if (model.getFirstName() == null) {
             return RestResultModel.buildError("PUT", "person", "FirstName should not be null");
         }
+        // Check required parameters (lastName)
         if (model.getLastName() == null) {
             return RestResultModel.buildError("PUT", "person", "LastName should not be null");
         }
 
+        // Retrieve the existing person by it's name
         Person existing = this.personRepo.findOneByFirstNameAndLastName(model.getFirstName(), model.getLastName());
 
+        // Check that the person already exist
         if (existing != null) {
+            // Get the update fireStation or the old one
             FireStation fireStation = model.getFireStation() != null ?
                     this.fireStationRepo.findOneByStation(model.getFireStation())
                     : existing.getFireStation();
+            // Check if the fireStation exist
             if (fireStation != null) {
                 try {
+                    // Build Person entity from Model
                     Person toUpdate = Person.fromModelAndDefault(model, existing);
+
+                    // Set it the fireStation (also for the equal check)
                     toUpdate.setFireStation(fireStation);
+
+                    // Check if they're is some changes
                     if (existing.equals(toUpdate)) {
+                        // if no change throw an error
                         return RestResultModel.buildError("PUT", "person", "Fail : no change");
                     }
 
                     personRepo.update(toUpdate);
 
+                    // success
                     return RestResultModel.buildSuccess("PUT", "person");
                 } catch (Exception e) {
+                    // Throw an error
                     return RestResultModel.buildError("PUT", "person", e.getMessage());
                 }
             } else {
+                // else Throw an error
                 return RestResultModel.buildError("PUT", "person", "The fireStation was not found");
             }
         } else {
+            // else Throw an error
             return RestResultModel.buildError("PUT", "person", "The combination of firstName and lastName was not found");
         }
     }
